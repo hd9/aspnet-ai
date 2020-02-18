afb81e0f2d1e0d2be7bd9bfad8734308dc338703
Bruno Hildenbrand <hd9x@outlook.com>
2020-02-18 15:07:58 -0700

    More updates

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # aspnet-ai
-Enabling Application Insights telemetry on an ASP.NET Core web applications
+Sample code on how to enable Application Insights telemetry on an ASP.NET Core web applications.
+
+Fore more information, visit: [Adding Application Insights telemertry to your
+ASP.NET Core website | HildenCo](https://blog.hildenco.com/2020/03/adding-application-insights-telemetry.html)
+
+## Running
+```
+git clone https://github.com/hd9/aspnet-ai.git
+cd aspnet-ai
+dotnet run
+```
+
+Don't forget to add your AppInsights telemetry key on `appSettings.json`.
+
+## Thanks!
+And don't forget to visit [blog.hildenco.com](https://blog.hildenco.com).
+
+## License
+MIT
---
+++ src/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.ApplicationInsights;
+using Microsoft.AspNetCore.Diagnostics;
+using aspnet_ai.Models;
+
+namespace aspnet_ai.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly TelemetryClient _telemetry;
+
+        public HomeController(ILogger<HomeController> logger, TelemetryClient telemetry)
+        {
+            _telemetry = telemetry;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            _telemetry.TrackEvent($"Privacy requested @ {DateTime.UtcNow} UTC");
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            // IExceptionHandlerPathFeature requires Microsoft.AspNetCore.Diagnostics
+            // run: dotnet add package Microsoft.AspNetCore.Diagnostics --version 2.2.0
+            var error = HttpContext.Features.Get<IExceptionHandlerFeature>();
+            if (error != null)
+            {
+                _telemetry.TrackException(error.Error); 
+            }
+
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
